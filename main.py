38a23d911e3214fd36f325ff718825a262637ac9
GALGALLOR <69774313+GALGALLOR@users.noreply.github.com>
2021-12-26 11:09:18 +0300

    basics

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from flask import Flask,url_for,redirect,render_template,request
+
+app=Flask(__name__)
+app.secret_key='hey'
+
+
+@app.route('/')
+def free():
+    return url_for('home')
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
