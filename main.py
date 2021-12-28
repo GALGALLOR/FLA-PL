f1915b31b626062165a13456b87c2e3ea6693ec9
GALGALLOR <69774313+GALGALLOR@users.noreply.github.com>
2021-12-28 18:21:05 +0300

    big step

--- main.py
+++ main.py
@@ -21,6 +21,204 @@ app.config['MYSQL_PASSWORD']='GALGALLO10'
 app.config['MYSQL_DB']='FLAPL'
 
 
+@app.route('/team',methods=['GET','POST'])
+def team():
+    username=session['username']
+    goalkeepers=[]
+    defenders=[]
+    midfielders=[]
+    attackers=[]
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM '+username +' WHERE ROLE="GOALKEEPER" AND STATUS="ON"')
+        goalkeeper=cursor.fetchall()
+        for gk in goalkeeper:
+            goalkeepers.append(gk)
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM '+username +' WHERE ROLE="DEFENDER" AND STATUS="ON"')
+        defender=cursor.fetchall()
+        for cb in defender:
+            defenders.append(cb)
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM '+username +' WHERE ROLE="MIDFIELDER" AND STATUS="ON"')
+        midfielder=cursor.fetchall()
+        for mid in midfielder:
+            midfielders.append(mid)
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM '+username +' WHERE ROLE="ATTACKER" AND STATUS="ON"')
+        attacker=cursor.fetchall()
+        for st in attacker:
+            attackers.append(st)
+    miraa=goalkeepers,defenders,midfielders,attackers
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM '+username +' WHERE STATUS="SUBSTITUTE"')
+        substitute=cursor.fetchall()
+        
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM PLAYERDATA')
+        players=[]
+        player=cursor.fetchall()
+        for k in player:
+            players.append(k)
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM '+username+' WHERE STATUS="ON"')
+        playing=cursor.fetchall()
+    
+    if request.method=='POST':
+        submit=str(request.form['submit'])
+        if submit=='substitute':
+            subout=str(request.form['on'])
+            subin=str(request.form['substitute'])
+            cursor=mydb.connection.cursor()
+            cursor.execute('UPDATE '+username+' SET STATUS="ON" WHERE NAME="'+subin+'"')
+            cursor.execute('UPDATE '+username+' SET STATUS="SUBSTITUTE" WHERE NAME="'+subout+'"')
+            mydb.connection.commit()
+        else:
+            player=str(request.form['sell'])
+            cursor=mydb.connection.cursor()
+            cursor.execute('DELETE FROM '+username+' WHERE NAME="'+player+'"')
+            mydb.connection.commit()
+
+            with app.app_context():
+                cursor=mydb.connection.cursor()
+                cursor.execute('SELECT WORTH FROM PLAYERDATA WHERE NAME="'+player+'"')
+                worth=cursor.fetchall()
+                cash=[]
+                for worth in worth:
+                    for worth in worth:
+                        cash.append(worth)
+                print(cash[0])
+            with app.app_context():
+                cursor=mydb.connection.cursor()
+                cursor.execute('SELECT COINS FROM USERDATA WHERE USERNAME="'+username+'"')
+                coin=cursor.fetchall()
+                coins=[]
+                for coin in coin:
+                    for coin in coin:
+                        coins.append(coin)
+
+                coins=int(coins[0])+int(cash[0])
+                print(coins)
+            cursor=mydb.connection.cursor()
+            cursor.execute('UPDATE USERDATA SET COINS="'+str(coins)+'" WHERE USERNAME="'+username+'"')
+            mydb.connection.commit()
+
+
+            
+    else:
+        message='Welcome back'
+        print(message)
+    return render_template('team.html',playing=playing,substitutes=substitute,username=username,players=players,goalkeepers=goalkeepers,midfielders=midfielders,defenders=defenders,attackers=attackers)
+
+@app.route('/transfers',methods=['POST','GET'])
+def transfers():
+    username=session['username']
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM PLAYERDATA')
+        xlo=cursor.fetchall()
+
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT * FROM '+username)
+        owned=cursor.fetchall()
+        names=[]
+        for x in owned:
+            for x in x:
+                names.append(x)
+
+    if request.method=='POST':
+        transaction=request.form['transaction']
+        player=request.form['player']
+        if transaction=='sell':
+            cursor=mydb.connection.cursor()
+            cursor.execute('DELETE FROM '+username+' WHERE NAME="'+player+'"')
+            mydb.connection.commit()
+
+            with app.app_context():
+                cursor=mydb.connection.cursor()
+                cursor.execute('SELECT WORTH FROM PLAYERDATA WHERE NAME="'+player+'"')
+                worth=cursor.fetchall()
+                cash=[]
+                for worth in worth:
+                    for worth in worth:
+                        cash.append(worth)
+                print(cash[0])
+            with app.app_context():
+                cursor=mydb.connection.cursor()
+                cursor.execute('SELECT COINS FROM USERDATA WHERE USERNAME="'+username+'"')
+                coin=cursor.fetchall()
+                coins=[]
+                for coin in coin:
+                    for coin in coin:
+                        coins.append(coin)
+
+                coins=int(coins[0])+int(cash[0])
+                print(coins)
+            cursor=mydb.connection.cursor()
+            cursor.execute('UPDATE USERDATA SET COINS="'+str(coins)+'" WHERE USERNAME="'+username+'"')
+            mydb.connection.commit()
+        else:
+            with app.app_context():
+                cursor=mydb.connection.cursor()
+                cursor.execute('SELECT * FROM PLAYERDATA WHERE NAME="'+player+'"')
+                player_data=cursor.fetchall()
+                playerdata=[]
+                for data in player_data:
+                    for data in data:
+                        playerdata.append(data)
+                print(playerdata)
+
+            cursor=mydb.connection.cursor()
+            cursor.execute('INSERT INTO '+username+'(NAME,AGE,ROLE,TEAM,HOUSE,POINTS,WORTH,STATUS)VALUES(%s,%s,%s,%s,%s,%s,%s,%s)',(playerdata[0],playerdata[1],playerdata[2],playerdata[3],playerdata[4],playerdata[5],playerdata[6],'on'))
+            mydb.connection.commit()
+
+            with app.app_context():
+                cursor=mydb.connection.cursor()
+                cursor.execute('SELECT WORTH FROM PLAYERDATA WHERE NAME="'+player+'"')
+                worth=cursor.fetchall()
+                cash=[]
+                for worth in worth:
+                    for worth in worth:
+                        cash.append(worth)
+            
+            with app.app_context():
+                cursor=mydb.connection.cursor()
+                cursor.execute('SELECT COINS FROM USERDATA WHERE USERNAME="'+username+'"')
+                coin=cursor.fetchall()
+                coins=[]
+                for coin in coin:
+                    for coin in coin:
+                        coins.append(coin)
+            
+            money=int(coins[0])-int(cash[0])
+            print(money)
+
+            cursor=mydb.connection.cursor()
+            cursor.execute('UPDATE USERDATA SET COINS="'+str(money)+'" WHERE USERNAME="'+username+'"')
+            mydb.connection.commit()
+
+    return render_template('transfers.html',username=username,xlo=xlo,owned=owned,names=names)
+
+@app.route('/league')
+def league():
+    with app.app_context():
+        cursor=mydb.connection.cursor()
+        cursor.execute('SELECT *, ROW_NUMBER() OVER(ORDER BY POINTS) AS ROW_NUM FROM LEAGUE ORDER BY ROW_NUM DESC')
+        teams=cursor.fetchall()
+        print(teams)
+    return render_template('league.html',teams=teams)
+
+@app.route('/accounts')
+def accounts():
+    return render_template('accounts.html')
+
 @app.route('/')
 def base():
     return redirect(url_for('home'))
@@ -72,7 +270,10 @@ def login():
                     if username in sample_name:
                         session['username']=username
                         session['password']=password
-
+                        with app.app_context():
+                            cursor=mydb.connection.cursor()
+                            cursor.execute('CREATE TABLE '+username+'(NAME VARCHAR(255),AGE VARCHAR(255),ROLE VARCHAR(255),TEAM VARCHAR(255),HOUSE VARCHAR(255),POINTS VARCHAR(255),WORTH VARCHAR(255))')
+                            mydb.connection.commit()
                         return redirect(url_for('home'))
                     else:
                         message='The Username or Password does nnot exist'
